88f72b303546cdeaea8f9a1e7fbe86e060f0b01c
Ashkan Feyzollahi <ashkanfeyzollahi@gmail.com>
2025-12-17 23:01:08 +0330

    feat(gc)!: add utility functions and switch remove_ptr to index-based

--- include/memory/gc.h
+++ include/memory/gc.h
@@ -1,6 +1,7 @@
 #ifndef MEMORY_GC_H
 #define MEMORY_GC_H
 
+#include <stdbool.h>
 #include <stddef.h>
 
 void *xngcrealloc(void *__ptr, size_t __size);
@@ -13,7 +14,10 @@ typedef struct gc_list
 
 extern gc_list gc_list_;
 void gc_list_add_ptr(void *ptr);
-void gc_list_remove_ptr(void *ptr);
+int32_t gc_list_contains(void *ptr);
+bool gc_list_update_ptr(void *ptr_a, void *ptr_b);
+void gc_list_swap_ptrs(int32_t ptr_a_index, int32_t ptr_b_index);
+void gc_list_remove_ptr(int32_t ptr_index);
 void collect_garbages();
 
 #endif
--- src/memory/gc.c
+++ src/memory/gc.c
@@ -1,4 +1,5 @@
 #include <errno.h>
+#include <stdbool.h>
 #include <stdint.h>
 #include <stdlib.h>
 
@@ -24,10 +25,10 @@ void gc_list_add_ptr(void *ptr)
     gc_list_.items[gc_list_.count++] = ptr;
 }
 
-void gc_list_remove_ptr(void *ptr)
+int32_t gc_list_contains(void *ptr)
 {
-    size_t index = -1;
-    for (int i = 0; i < gc_list_.count; i++)
+    int32_t index = -1;
+    for (size_t i = 0; i < gc_list_.count; i++)
     {
         if (gc_list_.items[i] == ptr)
         {
@@ -35,13 +36,35 @@ void gc_list_remove_ptr(void *ptr)
             break;
         }
     }
-    if (index == -1)
+    return index;
+}
+
+bool gc_list_update_ptr(void *ptr_a, void *ptr_b)
+{
+    int32_t ptr_a_index = gc_list_contains(ptr_a);
+    if (ptr_a_index == -1)
+        return false;
+    gc_list_.items[ptr_a_index] = ptr_b;
+    return true;
+}
+
+void gc_list_swap_ptrs(int32_t ptr_a_index, int32_t ptr_b_index)
+{
+    void *ptr_a = gc_list_.items[ptr_a_index];
+    gc_list_.items[ptr_a_index] = gc_list_.items[ptr_b_index];
+    gc_list_.items[ptr_b_index] = ptr_a;
+}
+
+void gc_list_remove_ptr(int32_t ptr_index)
+{
+    if (ptr_index == -1)
         return;
-    void *last_ptr = gc_list_.items[gc_list_.count - 1];
-    if (last_ptr != ptr)
+    int32_t last_ptr_index = gc_list_.count - 1;
+    if (ptr_index != last_ptr_index)
     {
-        gc_list_.items[gc_list_.count - 1] = ptr;
-        gc_list_.items[index] = last_ptr;
+        void *last_ptr = gc_list_.items[last_ptr_index];
+        gc_list_.items[last_ptr_index] = gc_list_.items[ptr_index];
+        gc_list_.items[ptr_index] = last_ptr;
     }
     gc_list_.items = xngcrealloc(gc_list_.items, sizeof(void *) * --gc_list_.count);
 }
